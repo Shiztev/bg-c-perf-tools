ab9cd7af02df7f308362d96e7c319f2dd144193d
Stevie Alvarez <stevie.6strings@gmail.com>
2023-06-02 16:50:07 -0400

    organize for top-down approach, fix tep_handle typo

--- latency/pflat.c
+++ latency/pflat.c
@@ -17,10 +17,46 @@
 #define PF_NAME "page_fault_lat"
 #define SYNTH_OP "synthetic"
 
+/**
+ * @brief Create a synthetic event to measure latency of page faults.
+ * 
+ * @param tep Tep handle that lies within scope
+ * @return struct tracefs_synth* must be freed
+ */
+struct tracefs_synth *make_event(struct tep_handle *tep)
+{
+  struct tracefs_synth *synth;
+
+  synth = tracefs_synth_alloc(tep, PF_NAME,
+                        NULL, "mmap_lock_start_locking",
+                        NULL, "mmap_lock_released",
+                        "common_pid", "common_pid",
+                        "pid");
+
+  // microsecond time difference
+  tracefs_synth_add_compare_field(synth, TRACEFS_TIMESTAMP_USECS,
+                          TRACEFS_TIMESTAMP_USECS,
+                          TRACEFS_SYNTH_DELTA_END, "delta");
+  return synth;
+}
+
+static int callback(struct tep_event *event, struct tep_record *record,
+                int cpu, void *data)
+{
+  static struct trace_seq s;
+
+  if (!s.buffer)
+    trace_seq_init(&s);
+  trace_seq_reset(&s);
+  tep_print_event(event->tep, &s, record, "%s\n", TEP_PRINT_INFO);
+  trace_seq_do_printf(&s);
+  return 0;
+}
+
 int main(int argc, char const *argv[])
 {
   struct tracefs_synth *synth;
-  struct temp_handle *tep;
+  struct tep_handle *tep;
   int val;
   int seconds;
 
@@ -51,41 +87,4 @@ int main(int argc, char const *argv[])
   tracefs_synth_destroy(synth);
   tracefs_synth_free(synth);
   return EXIT_SUCCESS;
-}
-
-/**
- * @brief Create a synthetic event to measure latency of page faults.
- * 
- * @param tep Tep handle that lies within scope
- * @return struct tracefs_synth* must be freed
- */
-struct tracefs_synth *make_event(struct tep_handle *tep)
-{
-  struct tracefs_synth *synth;
-
-  synth = tracefs_synth_alloc(tep, PF_NAME,
-                        NULL, "mmap_lock_start_locking",
-                        NULL, "mmap_lock_released",
-                        "common_pid", "common_pid",
-                        "pid");
-
-  // microsecond time difference
-  tracefs_synth_add_compare_field(synth, TRACEFS_TIMESTAMP_USECS,
-                          TRACEFS_TIMESTAMP_USECS,
-                          TRACEFS_SYNTH_DELTA_END, "delta");
-  return synth;
-}
-
-
-static int callback(struct tep_event *event, struct tep_record *record,
-                int cpu, void *data)
-{
-  static struct trace_seq s;
-
-  if (!s.buffer)
-    trace_seq_init(&s);
-  trace_seq_reset(&s);
-  tep_print_event(event->tep, &s, record, "%s\n", TEP_PRINT_INFO);
-  trace_seq_do_printf(&s);
-  return 0;
 }
