dc04c45f76926be80059be9459b7952012238889
Stevie Alvarez <stevie.6strings@gmail.com>
2023-06-06 12:30:11 -0400

    init commit

---
+++ opensnoop.c
@@ -0,0 +1,9 @@
+/**
+ * @file opensnoop.c
+ * @author Stevie Alvarez (steviea@google.com)
+ * @brief Trace open() syscalls and the respective file.
+ * @version 0.1
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
